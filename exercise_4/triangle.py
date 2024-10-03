0268b1968b8c74e542a08b60fb1f6aea51e361eb
Akshay879 <akshayumesh2000@gmail.com>
2024-10-03 17:41:40 -0400

    completed files

---
+++ exercise_4/triangle.py
@@ -0,0 +1,16 @@
+import math
+class Triangle():
+    def __init__(self,base,height):
+        self.base = base
+        self.height = height
+
+    def hypotenuse(self):
+        return math.sqrt(self.base**2 + self.height**2)
+    
+    def area(self):
+        return 1/2 * self.base * self.height
+    
+    def __str__(self):
+        print(f"Triangle: base={self.base}, height={self.height}")
+
+    
