48915b9ffacf871da16f39efef8fddbe301fc1c2
Akshay879 <akshayumesh2000@gmail.com>
2024-10-15 16:58:17 -0400

    created customers json file

--- midterm_project/midterm_project.py
+++ midterm_project/midterm_project.py
@@ -5,6 +5,7 @@ def parse_orders(filename):
     assert os.path.exists(filename), f"File '{filename}' does not exist."
     with open(filename, 'r') as f:
         orders = json.load(f)
-    print(orders)
-
-parse_orders("example_orders.json")
+    
+    customers = {order["phone"]: order["name"] for order in orders}
+    with open('customers.json', 'w') as f:
+        json.dump(customers, f, indent=4)
