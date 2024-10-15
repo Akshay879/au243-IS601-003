c8a843d5cbb9cff0da82dcf74374ab03b7011ef6
Akshay879 <akshayumesh2000@gmail.com>
2024-10-15 17:12:43 -0400

    created items.json with updated counts

--- midterm_project/midterm_project.py
+++ midterm_project/midterm_project.py
@@ -9,3 +9,19 @@ def parse_orders(filename):
     customers = {order["phone"]: order["name"] for order in orders}
     with open('customers.json', 'w') as f:
         json.dump(customers, f, indent=4)
+
+    summary = {}
+    for order in orders:
+        for item in order['items']:
+            item_name = item['name']
+            item_price = item['price']
+
+            if item_name in summary:
+                summary[item_name]['orders'] += 1
+            else:
+                summary[item_name] = {'price': item_price, 'orders': 1}
+
+    with open('items.json','w') as f:
+        json.dump(summary,f,indent=4)
+
+parse_orders('example_orders.json')
